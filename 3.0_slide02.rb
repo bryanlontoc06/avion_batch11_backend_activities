38579d6765647cffac483ea46df551f7e687fc8c
LAPTOP-CTUKF4T0\Ahyan <bryanlontoc06@gmail.com>
2021-11-17 22:36:05 +0800

    - updated activity file names.

--- 3.0_slide02.rb
+++
@@ -1,8 +0,0 @@
-def ltr(time)
-    return (time * 0.5).floor
-end
-
-
-puts ltr(3)
-puts ltr(6.7)
-puts ltr(11.8)
--- find_the_unique_number.rb
+++ find_the_unique_number.rb
@@ -1,4 +1,3 @@
-# Output all unique numbers (like .uniq method)
 def find_uniq(arr)
     arr.tally.select { |k, v| v == 1}.keys
 end
@@ -7,9 +6,12 @@ puts find_uniq([0, 0, 0, 0.55, 0, 0])
 
 
 
-# Output only first unique number
+puts "-"*60
+
+
+
 def find_uniq(arr) 
-arr.tally.index(1) 
+    arr.tally.index(1) 
 end
-puts find_uniq([1, 1, 1, 2, 1, 1, 3])
+puts find_uniq([1, 5, 1, 2, 1, 1, 3])
 puts find_uniq([0, 0, 0, 0.55, 0, 0])
